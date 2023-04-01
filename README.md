7d2219aec394635184acd5ca998c077f3f8ae8b8
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-04-01 13:04:29 +0900

    [MODIFY] boardState, Cell 데이터 구조 변경 이에 맞게 함수들 수정

--- README.md
+++ README.md
@@ -1,7 +1,93 @@
 # Directional-Test
 
-## 메인 트러블 슈팅
+# 메인 트러블 슈팅
 
-새로고침하면 오류뜨는 문제
+## 새로고침하면 오류뜨는 문제
+
 SSR 사용하여 query로 받은 boardSize, winTarget을 props로 넘겨주고
 Hydration 문제를 야기시키는 randomStartPlayer 함수를 서버사이드로 옮겨 시작 플레이어를 서버에서 랜덤으로 정해줌
+
+## 게임저장하고 불러올때 몇번째 턴인지 표시하는 기능
+
+boardHistory로 게임 보드 상태를 관리했기에 게임 저장 자체는 어렵지 않았으나
+각 기호위에 해당 기호가 놓인 차례를 표시해주는 기능을 구현하는데 어려움을 겪었다.  
+게임 저장 기능만 생각해두고 턴 정보를 관리하지 않아서 생긴 문제였다.
+
+생각해본 해결법은
+
+1. 2차원 배열 boardState의 row-col 위치에 턴 정보도 같이 저장한다.
+
+   1. 게임 턴수를 관리하는 state를 만든다.
+   2. handleCellClick 함수에서 boardHistory에 push할때 턴 정보를 같이 넣어주는 방법
+   3. 기존 2차원 배열의 boardState 는 [[O, X, O]... [...]]
+   4. 현재 플레이어 정보만 있는 string에서 object로 변경한다.
+   5. 여기에 턴 정보를 추가해
+   6. ```typescript
+      [
+        [{"player" : "O", "turn" : 1},{"player" : "X", "turn" : 2},{"player" : "O", "turn" : 3}]
+        ...
+        [...]
+        ]
+
+      ```
+
+      방식으로 만든다.
+
+2. 턴수를 관리하는 2차원 배열을 만들고 boardState가 마킹될때 동일한 위치에 턴 수를 저장한다
+   1. 결과적으로 동일한 크기, 형태의 2차원 배열이 만들어지며 존재하는 데이터만 달라진다.
+
+첫번째 방법은 기존의 boardState의 구조를 변경해야하지만 메모리 낭비가 덜하고 두번째 방법은 기존의 boardState를 변경하지 않지만 메모리 낭비가 더 많다.
+
+턴 정보와 플레이어를 함께 저장하는 첫번째 방법이 더 좋은 방법이라고 생각하여 첫번째 방법으로 구현하였다.
+
+1. - [x] Cell 타입 설정, BoardState 타입 수정
+
+2. - [x] 새로워진 구조에 맞게 initialBoardState 함수 수정
+3. - [x] currentTurn state 추가
+
+4. - [ ] handleCellClick 함수 수정
+   1. - [x] winner나 위치에 플레이어가 있을때 return
+   2. - [x] row-col 위치에 플레이어 정보와 턴 정보 저장
+   3. - [x] 위 과정이 끝나면 turn + 1
+5. - [x] checkWinner 함수 수정
+   1. - [x] row-col 위치를 검사할때 객체의 player 키값을 검사하도록 수정
+6. - [x] 렌더링 부분 수정
+   1. - [x] row-col위치의 객체의 player 키값을 렌더링하도록 수정
+
+### 버그
+
+첫번째 클릭에 가로열이 모두 클릭되어 첫번째 플레이어가 무조건 승리하는 버그가 있었다.
+initialBoardState를 생성할때 .fill({player:null, turn:null})로 초기화를 하였는데  
+이렇게 하면 모든 객체가 같은 주소를 가리키게 되어서 생긴 문제였다.
+
+```typescript
+const initialBoardState: BoardState = new Array(boardSize)
+  .fill(null)
+  .map(() => new Array(boardSize).fill({ player: null, turn: null }));
+
+const [boardState, setBoardState] = useState<BoardState>(initialBoardState);
+
+const handlCellClick = (row: number, col: number) => {
+  const newBoardState = boardState.map((row) => [...row]);
+  newBoardState[row][col].player = "O";
+  newBoardState[row][col].turn = 1;
+};
+```
+
+map 함수는 새로운 배열을 만들어서 그 안에 각각의 원소를 변환하는 함수를 실행한 결과를 할당한다.  
+하지만 배열 안에 있는 객체는 레퍼런스(참조) 타입으로, 배열 안에 있는 객체를 직접 수정하면 해당 객체의 참조가 저장된 모든 변수나 상수에서 해당 객체의 변화를 볼 수 있다.
+
+즉, 현재 코드에서 newBoardState는 boardState와 같은 객체를 가리키고 있고 따라서 newBoardState[row][col]로 선택한 객체의 값을 바꿔도 boardState의 해당 위치 객체도 같은 참조이기 때문에 함께 변경되기때문에 위와같은 버그가 발생했다.
+
+따라서 map을 이용하여 새로운 객체를 생성하는 것이 아니라, 다음과 같이 해당 위치의 객체를 복사한 새로운 객체를 만들어서 변경해야 한다
+
+```typescript
+const handlCellClick = (row: number, col: number) => {
+  const newBoardState = boardState.map((row) => row.map((col) => ({ ...col })));
+  newBoardState[row][col].player = "O";
+  newBoardState[row][col].turn = 1;
+  setBoardState(newBoardState);
+};
+```
+
+불변성을 위지하기 위해 spread operator를 사용했으며 이렇게하면 `newBoardState`는 `boardState` 와 다른 참조를 가지게되어 연관성이 없는 독립적인 객체가 된다.
--- temp.tsx
+++ temp.tsx
@@ -4,11 +4,6 @@ import useModal from "@/hooks/useModal";
 import GameEnd from "@/components/modals/GameEnd";
 import { GetServerSidePropsContext } from "next";
 
-/**
- * @description null은 아직 놓이지 않은 상태.
- * @description "X"는 플레이어 1의 표시.
- * @description "O"는 플레이어 2의 표시.
- */
 type CellValue = "X" | "O" | null;
 
 interface Cell {
@@ -17,9 +12,7 @@ interface Cell {
 }
 
 type BoardState = Cell[][];
-/**
- * @description undoCount는 각 플레이어가 undo를 몇번 했는지를 저장.
- */
+
 interface UndoCountProps {
   X: number;
   O: number;
@@ -35,9 +28,6 @@ export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   const { boardSize, winTarget } = context.query;
-  /**
-   * @description 랜덤으로 플레이어를 정함.
-   */
 
   const randomStartPlayer = Math.random() < 0.5 ? "X" : "O";
 
@@ -63,14 +53,11 @@ const GamePlay = (props: ServerSideProps) => {
     Number(queryWinTarget || props.winTarget)
   );
 
-  /**
-   * @description 게임 보드의 초기 상태를 생성.
-   * @description 게임 보드의 크기는 boardSize * boardSize.
-   * @description 게임 보드의 각 칸은 null로 초기화.
-   */
   const initialBoardState: BoardState = new Array(boardSize)
     .fill(null)
-    .map(() => new Array(boardSize).fill({ player: null, turn: null }));
+    .map(() =>
+      new Array(boardSize).fill(null).map(() => ({ player: null, turn: null }))
+    );
 
   const [boardState, setBoardState] = useState<BoardState>(initialBoardState);
   const [currentPlayer, setCurrentPlayer] = useState<CellValue>(
@@ -81,44 +68,22 @@ const GamePlay = (props: ServerSideProps) => {
     X: 0,
     O: 0,
   });
-  /**
-   * @description 게임 보드의 이전 상태를 저장.
-   * @description 맨 처음에 보드의 초기상태를 저장하고 이후로 마킹이 될때마다 생긴 보드의 상태를 배열로 쌓는다.
-   */
+
   const [boardHistory, setBoardHistory] = useState<BoardState[]>([
     initialBoardState,
   ]);
 
-  /**
-   * @description 보드의 턴 수를 증가시킴, boardSize * boradSize 보다 커지지 못하게 함.
-   * @param boardSize {number} 게임 보드의 가로, 세로 크기
-   */
   const handleTurnUp = (boardSize: number) => {
     const maxTurn = boardSize * boardSize;
     if (currentTurn > maxTurn) return;
     setCurrentTurn((prev) => prev + 1);
   };
 
-  /**
-   * @description 게임 보드의 가로, 세로, 대각선 방향을 검사하여 승리자를 찾음.
-   * @param board {BoardState} 게임 보드
-   * @returns {CellValue} 승리자의 표시. 승리자가 없으면 null.
-   */
   const checkWinner = (board: BoardState): CellValue => {
-    /**
-     * @description 가로 방향의 승리 조건을 만족하는지 검사.
-     * @description winTarget이 3이고, boardSize가 5이라고 하면 가로에서는 해당 가로열을 모두 검색할 필요 없이 시작하는 부분의 열 부터 3개의 열을 검사.(winTarget)
-     * @example [o, o, x ,x ,x] 를 검사할때 index 0 번의 o는 index 4의 x 까지 검사할 필요 없이 index 2의 x 까지만 검사하면 됨.
-     * @example index 2번의 x를 검사할때 연속한 3개까지인 index 4번의 x까지 검사하면 되고 이 경우 승리 조건을 만족함.
-     */
     for (let row = 0; row < boardSize; row++) {
       for (let col = 0; col <= boardSize - winTarget; col++) {
         const winRow = board[row].slice(col, col + winTarget);
 
-        /**
-         * @description winRow의 모든 요소가 같은 값이고 null이 아닌 경우 승리 조건을 만족함.
-         * @returns {CellValue} 승리자의 표시.
-         */
         if (
           winRow.every(
             (cell) => cell.player === winRow[0].player && cell.player !== null
@@ -129,25 +94,12 @@ const GamePlay = (props: ServerSideProps) => {
       }
     }
 
-    /**
-     * @description 세로 방향에서는 가로방향과 다르게 col, row 순서로 검사.
-     * @description 가로 방향과 마찬가지로 winTarget이 3이고, boardSize가 5이라고 하면 세로에서는 해당 세로열을 모두 검색할 필요 없이 시작하는 부분의 행 부터 3개의 행을 검사.(winTarget)
-     * @example [[o,x,o,x,o,x],
-     *           [o,x,o,x,o,x],
-     *           [o,x,o,x,o,x],
-     *           [o,x,o,x,o,x],
-     *           [o,x,o,x,o,x]] 를 검사할때 세로 방향을 0 ~ 4 번의 index로 잡는다면 0번 index의 o는 2번 index의 o까지만 검사하면 그 이후 요소들은 검사할 필요가 없음.
-     */
     for (let col = 0; col < boardSize; col++) {
       for (let row = 0; row <= boardSize - winTarget; row++) {
         let sameValue = true;
         const firstCellValue = board[row][col].player;
         if (firstCellValue === null) continue;
 
-        /**
-         * @description 세로 방향으로 한개씩 index를 올리면서 firstCellValue와 일치는지 검사.
-         * @description 일치하지 않는 경우 sameValue를 false로 변경하고 break.
-         */
         for (let i = 1; i < winTarget; i++) {
           if (board[row + i][col].player !== firstCellValue) {
             sameValue = false;
@@ -155,20 +107,12 @@ const GamePlay = (props: ServerSideProps) => {
           }
         }
 
-        /**
-         * @description 위의 검사를 통과해 sameValue가 true로 유지된다면 승리 조건을 만족함.
-         * @returns {CellValue} 승리자의 마크. "X" or "O"
-         */
         if (sameValue) {
           return firstCellValue;
         }
       }
     }
 
-    /**
-     * @description 왼쪽 대각선 방향 검사
-     * @description 세로방향 검사와 비슷
-     */
     for (let row = 0; row <= boardSize - winTarget; row++) {
       for (let col = 0; col <= boardSize - winTarget; col++) {
         let sameValue = true;
@@ -188,10 +132,6 @@ const GamePlay = (props: ServerSideProps) => {
       }
     }
 
-    /**
-     * @description 오른쪽 대각선 방향 검사
-     * @description 세로방향 검사와 비슷
-     */
     for (let row = 0; row <= boardSize - winTarget; row++) {
       for (let col = winTarget - 1; col < boardSize; col++) {
         let sameValue = true;
@@ -215,9 +155,6 @@ const GamePlay = (props: ServerSideProps) => {
   };
 
   const handleUndo = () => {
-    /**
-     * @description undoCount가 3이상이면 더이상 undo를 할 수 없음.
-     */
     if (undoCount[currentPlayer!] > 3) return;
 
     if (boardHistory.length > 1) {
@@ -235,39 +172,21 @@ const GamePlay = (props: ServerSideProps) => {
     }
   };
 
-  /**
-   * @description 셀 클릭시 실행되는 함수.
-   * @description 보드의 마킹과, 보드의 히스토리를 관리, 플레이어 전환, 승리 여부를 판단.
-   * @param {number} row - 클릭한 셀의 행 번호.
-   * @param {number} col - 클릭한 셀의 열 번호.
-   */
   const handleCellClick = (row: number, col: number) => {
-    /**
-     * @description 이미 누군가가 놓았거나, 이미 승자가 결정되었으면 더 이상 놓을 수 없음.
-     */
     if (winner || boardState[row][col].player) {
       return;
     }
 
-    /**
-     * @description 새로운 보드 상태를 생성.
-     * boardState를 직접 수정하면 생길 수 있는 문제를 방지하기위해, boardState와 동일한 배열을 생성하고 생성하고, 그 배열을 수정.
-     * @description 새로운 보드 상태를 boardHistory에 추가. (이전 상태를 보관하는 로직 추가)
-     * @description 새로운 보드 상태를 boardState에 저장.
-     */
     const newBoardState = boardState.map((row) =>
-      row.map((col) => ({ ...col }))
+      row.map((cell) => ({ ...cell }))
     );
+
     newBoardState[row][col].player = currentPlayer;
     newBoardState[row][col].turn = currentTurn;
     const newBoardHistory = [...boardHistory, newBoardState];
     setBoardState(newBoardState);
     setBoardHistory(newBoardHistory);
 
-    /**
-     * @description 승자가 결정되었는지 확인
-     * @description 승자가 결정되었으면 승자를 표시하고, 아니면 플레이어를 전환.
-     */
     const gameWinner = checkWinner(newBoardState);
     if (gameWinner) {
       setWinner(gameWinner);
@@ -304,9 +223,6 @@ const GamePlay = (props: ServerSideProps) => {
     setWinner(checkWinner(boardState));
   }, [boardState]);
 
-  /**
-   * @description 승자가 결정되면, 승자를 표시하는 모달을 띄움.
-   */
   useEffect(() => {
     if (winner) {
       const showEndGameModal = () => {
